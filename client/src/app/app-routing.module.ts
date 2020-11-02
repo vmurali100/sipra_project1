bb6f85f302f632972ee29322ed7d68a50e7a24b2
vmurali100 <vmurali100@gmail.com>
2020-11-02 15:08:19 +0530

    Adding Basic Setup

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
