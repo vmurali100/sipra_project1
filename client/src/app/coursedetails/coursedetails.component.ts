f659869fac34fca1411c7243911964fc2bd38d33
vmurali100 <vmurali100@gmail.com>
2020-11-08 20:30:03 +0530

    Adding Quiz

--- client/src/app/coursedetails/coursedetails.component.ts
+++ client/src/app/coursedetails/coursedetails.component.ts
@@ -8,6 +8,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CoursedetailsComponent implements OnInit {
   coursedetails;
+  startQuiz;
+  quizDetails :any = [];
+  currentQuestion ;
+  currentAnswers=[]
+  qIndex=0;
   python = null;
   javascript = null;
   DataScience = null;
@@ -17,6 +22,9 @@ export class CoursedetailsComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.quizDetails = quizQuestions;
+    this.currentQuestion = this.quizDetails[this.qIndex].question;
+    this.currentAnswers = this.quizDetails[this.qIndex].answers;
     this.activatedRoute.params.subscribe((res) => {
       this.coursedetails = res['course'];
       if (this.coursedetails == 'python') {
@@ -33,5 +41,39 @@ export class CoursedetailsComponent implements OnInit {
         this.machineLearning = true;
       }
     });
+    
+  }
+  startQuizEvent(){
+    this.startQuiz = true
+  }   
+  
+  submitAnswer(ans){
+    this.qIndex++;
+    this.currentQuestion = this.quizDetails[this.qIndex].question;
+    this.currentAnswers = this.quizDetails[this.qIndex].answers;
   }
 }
+
+const quizQuestions = [
+  {question:"Which of the following is not JavaScript Data Types?",
+  answers:["Undefined","Number","Boolen","Float"]},
+  {question:"Which company developed JavaScript?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:" Inside which HTML element do we put the JavaScript?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"Which of the following is correct about features of JavaScript?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"Choose the correct JavaScript syntax to change the content of the following HTML code.",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"Which of the following is the correct syntax to display 'Letsfindcourse' in an alert box using JavaScript?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"What is the correct syntax for referring to an external script called 'LFC.js'?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"Which of the following is not Javascript frameworks or libraries?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"Why so JavaScript and Java have similar name?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+  {question:"What is the original name of JavaScript?",
+  answers:["Netscape","Bell Labs","Sun Microsystems ","IBM"]},
+
+]
