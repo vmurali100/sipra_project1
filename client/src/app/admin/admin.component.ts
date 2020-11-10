67a954f3c963e34d34a39de029efc9095d508aa0
vmurali100 <vmurali100@gmail.com>
2020-11-10 22:07:12 +0530

    Added

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  showContenet=false
+  showContenet = null
   constructor() { }
 
   ngOnInit(): void {
